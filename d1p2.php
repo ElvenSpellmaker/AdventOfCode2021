434fe0ec8065c8dedf358e9d343f26beb7e39dd5
Jack Blower <Jack@elvenspellmaker.co.uk>
2021-12-01 17:37:10 +0000

    fix: Simplify d1p2 massively

--- d1p2.php
+++ d1p2.php
@@ -3,20 +3,10 @@
 $lines = explode("\n", file_get_contents('d1.txt'));
 
 $count = 0;
-$window = [];
-$window[] = array_shift($lines);
-$window[] = array_shift($lines);
-$window[] = array_shift($lines);
-foreach ($lines as $line)
+$linesCount = count($lines);
+for ($i = 0, $j = 3; $j < $linesCount; $i++, $j++)
 {
-	$count1 = array_sum($window);
-	array_shift($window);
-	$window[] = $line;
-	$count2 = array_sum($window);
-	if ($count2 > $count1)
-	{
-		$count++;
-	}
+	$count += $lines[$i] < $lines[$j];
 }
 
 echo $count, "\n";
